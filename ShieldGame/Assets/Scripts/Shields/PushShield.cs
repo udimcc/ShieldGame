3bc108eca81cda92ab3cf0649c4aef1f8e537b61
udim <97udi97@walla.com>
2020-09-11 14:36:24 +0300

    Push shield

---
+++ ShieldGame/Assets/Scripts/Shields/PushShield.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushShield : MonoBehaviour, IShield
+{
+    public void OnProjectileCollision(GameObject projectileObj)
+    {
+        Destroy(projectileObj);
+    }
+}
